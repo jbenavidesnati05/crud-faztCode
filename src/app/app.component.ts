9dfda643dd461b10048843461b6406c96d49abea
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-02-08 17:10:10 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { identifierName } from '@angular/compiler';
 import { Component } from '@angular/core';
+import { Empleado } from './models/empleado';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud-faztCode';
+  nombre= ""
+  empleadoArray:Empleado[]=
+  [
+    {id:1, name:"rian",country: "USA"},
+    {id:2, name:"ana",country: "ECU"},
+    {id:3, name:"juan",country: "BRA"},
+  ];
+
+  seleccionarEmpleado:Empleado = new Empleado();
+
+  abrirParaEditar(empleado:Empleado){
+    this.seleccionarEmpleado = empleado;
+  }
+  agregarEditar(){
+    if (this.seleccionarEmpleado.id == 0 && this.seleccionarEmpleado.name != "" && this.seleccionarEmpleado.country != "" ) {
+      this.seleccionarEmpleado.id = this.empleadoArray.length+1;
+      this.empleadoArray.push(this.seleccionarEmpleado)
+      console.log( this.seleccionarEmpleado.name);
+      console.log(this.seleccionarEmpleado.country);
+      console.log("agrego empleado");
+
+    } else {
+      this.seleccionarEmpleado = new Empleado();
+      alert("debe ingresar un nombre y pais")
+
+    }
+  }
+  eliminarEmpleado(){
+  if (confirm('¿Estas seguro de querer elimarlo? ') ) {
+    this.empleadoArray = this.empleadoArray.filter(x => x!=this.seleccionarEmpleado)
+    this.seleccionarEmpleado = new Empleado();
+  } else {
+    alert("no fue posible eliminarlo")
+  }
+
+
+  }
 }
